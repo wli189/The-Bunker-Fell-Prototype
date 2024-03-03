4bcaaf1d053dfe9f9bd69af2a703a5c3323c2769
eky2429 <eky2@uic.edu>
2024-03-03 10:25:04 -0600

    Added movement to player

--- The Bunker Fell/Assets/Scripts/Player.cs
+++ The Bunker Fell/Assets/Scripts/Player.cs
@@ -5,26 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     //Different speeds we can modify
-    public float speed = 5f;
+    public float speed = 20f;
     public float rotationSpeed = 90f;
 
     //Gets input
     private float horizontalInput;
     private float verticalInput;
 
-    //Directions and velocities
-    private Vector3 moveVelocity;
-    private Vector3 turnVelocity;
-    private Vector3 direction;
-
-    //Controller for player
-    private CharacterController controller;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        controller = GetComponent<CharacterController>();
-    }
+    void Start() {}
 
     // Update is called once per frame
     void Update()
@@ -33,17 +23,8 @@ public class Player : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        //Sets direction
-        direction = new Vector3(horizontalInput, 0, verticalInput);
-        direction.Normalize();
-
-        //Sets velocity
-        moveVelocity = transform.forward * speed * verticalInput;
-        turnVelocity = transform.up * rotationSpeed * horizontalInput;
-
-
-        //Moves character (Movement and rotation wise)
-        controller.Move(moveVelocity * Time.deltaTime);
-        transform.Rotate(turnVelocity * Time.deltaTime);
+        //Move object
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
