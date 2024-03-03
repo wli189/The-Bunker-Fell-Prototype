94d0aa89b9a50c6b0f34c45a57c81dabbcfbcd0f
eky2429 <eky2429@gmail.com>
2024-03-03 12:41:48 -0600

    Added proper play controls (including mouse movement)

---
+++ The Bunker Fell/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float mouseSenstivity = 100f;
+    public Transform playerBody;
+
+    private float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSenstivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSenstivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -45f, 45f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
